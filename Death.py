a27cc49c329a27c17a5b20d413aecf8004c70de5
valkyrie745 <134057300+valkyrie745@users.noreply.github.com>
2023-08-17 18:28:03 +0100

    Add files via upload
    Implemented a killing function and a new generation creation function. Still haven't added mutations so these are just new randomly created dots that haven't inherited anything from the survivors.

--- Death.py
+++ Death.py
@@ -31,4 +31,18 @@ def kill_left(dots):
         
     return dots
                        
-    
+def create_life(dots,n):
+    if (dots!=list):
+        oglen = n
+        newlen = len(dots)
+        ncreate = n - len(dots)
+        dotself = dots[0].__dict__
+        width = dotself['screen_width']
+        height = dotself['screen_height']
+        nsense = dotself['no_sensory_neurons']
+        nact = dotself['no_action_neurons']
+        nint = dotself['no_internal_neurons']
+            
+        newdots = [Dot(nsense, nact, nint, width, height) for _ in range(ncreate)]
+        dots.extend(newdots)
+    return dots
